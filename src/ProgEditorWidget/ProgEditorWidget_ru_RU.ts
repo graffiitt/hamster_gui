19cec311491b166ea21334d5d50a18a665fa5d83
vlad <vladikkopylov31@gmail.com>
2023-11-15 11:09:27 +0300

    update translate

--- src/ProgEditorWidget/ProgEditorWidget_ru_RU.ts
+++ src/ProgEditorWidget/ProgEditorWidget_ru_RU.ts
@@ -3,9 +3,40 @@
 <TS version="2.1" language="ru_RU">
 <context>
     <name>ProgrammEditorWidget</name>
+    
     <message>
         <source>add</source>
-        <translation >файл</translation>
+        <translation >добавить</translation>
+    </message>
+
+    <message>
+        <source>addLine</source>
+        <translation >добавить строку</translation>
+    </message>
+
+    <message>
+        <source>delete</source>
+        <translation >удалить</translation>
+    </message>
+
+    <message>
+        <source>move</source>
+        <translation >движение</translation>
+    </message>
+
+    <message>
+        <source>logic</source>
+        <translation >логика</translation>
+    </message>
+
+    <message>
+        <source>math</source>
+        <translation >математика</translation>
+    </message>
+
+    <message>
+        <source>overwrite</source>
+        <translation >перезаписать</translation>
     </message>
 
     </context>
